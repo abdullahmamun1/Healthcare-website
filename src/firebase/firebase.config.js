bf37924964f884d6f4059d9986f8ab0137c6c7bb
Abdullah Mamun <hello.abdullahmamun1@gmail.com>
2021-10-17 23:06:46 +0600

    installation complete

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyChEmj4Q4i6_lwTLr3wzo_iXi6qQ_nY_UY",
+    authDomain: "health-care-website-55917.firebaseapp.com",
+    projectId: "health-care-website-55917",
+    storageBucket: "health-care-website-55917.appspot.com",
+    messagingSenderId: "765977987283",
+    appId: "1:765977987283:web:39c4d93c1cf8edfa0f6c26"
+  };
+
+  export default firebaseConfig;
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import initializeAuthentication from '../../../react-emajohn/src/components/Login/Firebase/firebase.init';
+
+initializeAuthentication();
+
+const useFirebase = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default useFirebase;
